db6960a1535e62fee11f9bae09e392c4f2079a3e
Jeremy Chan Yue Nang <yiungnye@gmail.com>
2023-06-11 20:46:05 +0400

    working on home view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
 
   def home
+    @movies = Movie.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
+Movie.destroy_all
+
+puts "Creating movies"
+
+Fast_X = Movie.new(title: "Fast X", description: "Dom Toretto and his family are targeted by the vengeful son of drug kingpin Hernan Reyes.",
+                  release_date: Date.new(2023,5,19))
+
+fast_x_photo = URI.open("https://www.themoviedb.org/t/p/original/fiVW06jE7z9YnO4trhaMEdclSiC.jpg")
+Fast_X.photo.attach(io: fast_x_photo, filename: "nes.png", content_type: "image/png")
+Fast_X.save
