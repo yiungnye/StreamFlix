3a0096af2a223ba086fbf4964f12a0505f501fde
Jeremy Chan Yue Nang <yiungnye@gmail.com>
2023-06-11 18:21:21 +0400

    in movies controller added new and create actions, routed in routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,15 @@
 class MoviesController < ApplicationController
-  def index
+  def new
+    @movie = Movie.new
+  end
 
+  def create
+    @movie = Movie.new(movie_params)
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permits(:title, :description, :release_date, :photo)
   end
 end
